6cae2df40f446ac62741d4a1e3e8600d757e5d88
Christiaan Venter <59892042+codecventer@users.noreply.github.com>
2020-05-09 12:22:53 +0200

    Add files via upload

---
+++ build/precache-manifest.5b7567bfa96d4798b0544a34d94b9880.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8f833ac6460667cfeda96ff92c3ddb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b2e1c9d362ee8b8a109",
+    "url": "/static/css/main.1530f4be.chunk.css"
+  },
+  {
+    "revision": "b396d1a1f3a0324e5e86",
+    "url": "/static/js/2.a0e1b077.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.a0e1b077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2e1c9d362ee8b8a109",
+    "url": "/static/js/main.5821785b.chunk.js"
+  },
+  {
+    "revision": "08be562c99265cff8e46",
+    "url": "/static/js/runtime-main.e069335c.js"
+  },
+  {
+    "revision": "ec0791621e5caf7cceb1fe015026ef9a",
+    "url": "/static/media/covid-19.ec079162.png"
+  }
+]);
